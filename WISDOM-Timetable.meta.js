c87864ab1ad7515bc24531a26cd06fbdb13f38ce
AJ-cubes <jaina10@wis.edu.hk>
2025-11-13 08:52:28 +0800

    Bug Fix - Version 2025.4.2.1

--- WISDOM-Timetable.meta.js
+++ WISDOM-Timetable.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WISDOM Timetable
 // @namespace    https://github.com/AJ-cubes/WISDOM-Timetable
-// @version      2025.4.2
+// @version      2025.4.2.1
 // @description  Enhances WISDOM Timetable with keyboard shortcuts and overlays: Alt+T opens a full‑screen view of today’s current and upcoming lessons with subject links, highlights the active period, and shows birthdays; Alt+P displays tomorrow’s timetable, required books, and PE status, with interactive book toggling to mark whether a book is needed. Open Tampermonkey and select the metadata to update.
 // @author       AJ-cubes
 // @match        *://*/*
--- WISDOM-Timetable.user.js
+++ WISDOM-Timetable.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WISDOM Timetable
 // @namespace    https://github.com/AJ-cubes/WISDOM-Timetable
-// @version      2025.4.2
+// @version      2025.4.2.1
 // @description  Enhances WISDOM Timetable with keyboard shortcuts and overlays: Alt+T opens a full‑screen view of today’s current and upcoming lessons with subject links, highlights the active period, and shows birthdays; Alt+P displays tomorrow’s timetable, required books, and PE status, with interactive book toggling to mark whether a book is needed. Open Tampermonkey and select the metadata to update.
 // @author       AJ-cubes
 // @match        *://*/*
@@ -55,8 +55,8 @@
     }
 
     function getDisplayName(code, room) {
-        if (classNames[code.replace(/[^a-zA-Z]/g, '').toUpperCase()]) return classNames[code.replace(/[^a-zA-Z]/g, '').toUpperCase()];
         if (CA_DICT[room.slice(0, 2)]) return CA_DICT[room.slice(0, 2)];
+        if (classNames[code.replace(/[^a-zA-Z]/g, '').toUpperCase()]) return classNames[code.replace(/[^a-zA-Z]/g, '').toUpperCase()];
         return code;
     }
 
@@ -486,9 +486,9 @@
 
                     for (const [key, value] of Object.entries(classDict)) {
                         if (
+                            (safeDisplayName && key.includes(safeDisplayName)) ||
                             (safeCode && key.includes(safeCode)) ||
-                            (safeRoom && key.includes(safeRoom)) ||
-                            (safeDisplayName && key.includes(safeDisplayName))
+                            (safeRoom && key.includes(safeRoom))
                         ) {
                             URLs.push(value);
                             found = true;
